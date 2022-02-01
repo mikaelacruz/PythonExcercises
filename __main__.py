e01d65827269e084b5d94ce448d832017da54384
Mikaela Cruz <82981743+mikaelacruz@users.noreply.github.com>
2022-01-31 23:17:04 -0600

    Added Contains Duplication problem

--- __main__.py
+++ __main__.py
@@ -81,10 +81,35 @@ return -1
 
 
 
+# EDGE CASES 0, 1, AND ALL SAME ELEMENTS
+    if len(s) == 0: return -1
+    if len(s) == 1: return 0
+    if len(set(s)) == 1: return -1
 
+################################################################################################
 
+CONTAINS DUPLICATION
 
+Given an integer array nums, return TRUE if any value appears at least twice in the array,
+return FALSE if every element is distinct
 
+# Input
+nums = [ 1, 2, 3, 1]
+counted_dict = {}
 
+# Looping through the input array/list
+for num in nums:
+# If the element is in the counted dict
+    if num in counted_dict:
+# Then we have a duplicate!!
+# Returning True
+        return True
+    # Else, this element is new and we add it to the counted dict
+    else:
+        counted_dict[num] = 1
+# We didn't find any duplicates at all :(
+return False
 
+# Output
+# Should be TRUE
 """
