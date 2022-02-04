77eee9255ef762c3d4b7162f73845202bb477fdb
Mikaela Cruz <82981743+mikaelacruz@users.noreply.github.com>
2022-02-04 14:33:31 -0600

    Refactored all names

--- __main__.py
+++ __main__.py
@@ -1,19 +1,19 @@
 import add_strings
-import anagram
+import dictionary_anagram
 import array_common_elements
 import array_largest_sum
 import array_maximum_subarray
 import array_most_frequently_occurring
 import array_pair_sum
 import array_rotation
-import best_time_to_buy_and_sell_stock
-import first_unique_character_in_string
-import longest_common_prefix
-import longest_substring
-import reverse_words
-import unique_character_in_string
-import valid_palindrome
-import valid_parentheses
+import dynamic_programming_best_time_to_buy_and_sell_stock
+import dictionary_first_unique_character_in_string
+import string_longest_common_prefix
+import string_longest_substring
+import string_reverse_words
+import string_unique_character_in_string
+import string_valid_palindrome
+import stack_valid_parentheses
 
 __name__ == '__main__'
 
